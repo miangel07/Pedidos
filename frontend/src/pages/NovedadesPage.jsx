209d2b838d6aeca54386b74764b3b5b4ec4826da
emersson <emerssonramirez510@gmail.com>
2024-10-26 20:59:02 -0500

    actualizar una solicitud en el modulo de novedades

--- frontend/src/pages/NovedadesPage.jsx
+++ frontend/src/pages/NovedadesPage.jsx
@@ -1,6 +1,8 @@
 import { Button, Chip } from "@nextui-org/react";
 import { Layout } from "../components/layouts/Layout";
 import { Table } from "../components/Nextui/Table";
+import Modals from "../components/subcomponents/Modal";
+import { ContenidoNovedad } from "../components/subcomponents/DetalleNovedad";
 
 /* import { CambiarEstadoDomiciliario } from "../components/subcomponents/EstadoDomiciliario"; */
 import { toast } from "react-toastify";
@@ -9,21 +11,26 @@ import { toast } from "react-toastify";
 import { useState } from "react";
 
 // hooks
-import { useNovedadesQuery } from "../hooks/Novedades";
+import { useNovedadesQuery, useNovedadesIDQuery } from "../hooks/Novedades";
 import { useIncidenciasQuery } from "../hooks/Incidencias";
 import { axiosCliente } from "../service/axios";
 
 export const NovedadesPage = () => {
   const [cambiarTable, setcambiarTable] = useState(true);
 
+  //modal
+  const [isOpen, setIsOpen] = useState(false);
+
   // novedades
   const { novedades } = useNovedadesQuery();
 
   // incidencias
   const { incidencias, refreshDataIncidencias } = useIncidenciasQuery();
+  const { novedadesID, obtenerNovedades } = useNovedadesIDQuery();
 
   // novedades
   const columnas = [
+    "id",
     "descripcion",
     "estado",
     "fecha_reporte",
@@ -52,15 +59,20 @@ export const NovedadesPage = () => {
     setcambiarTable(false);
   };
 
+  const handleEdit = (id) => {
+    obtenerNovedades(id);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   const newDataNovedades = novedades.map((fila) => ({
     ...fila,
     acciones: (
       <>
-        {/*  <Modals
-          buttonModal={"fasd"}
-          componente={<>hola como estas</>}
-          tittleModal={"Detalle de la novedada"}
-        /> */}
+        <Button onClick={() => handleEdit(fila.id)}></Button>
       </>
     ),
   }));
@@ -89,7 +101,17 @@ export const NovedadesPage = () => {
   return (
     <>
       <Layout>
-        {/* <CambiarEstadoDomiciliario /> */}
+        {isOpen && (
+          <>
+            <Modals
+              visible={isOpen}
+              title={"Detalle de la novedad"}
+              closeModal={closeModal}
+            >
+              <ContenidoNovedad novedadesID={novedadesID} />
+            </Modals>
+          </>
+        )}
 
         <div className="h-full w-full">
           <div className="flex justify-end gap-2">
@@ -103,34 +125,35 @@ export const NovedadesPage = () => {
               cambiarTable
                 ? newDataNovedades
                 : incidencias.map((fila) => ({
-                  ...fila,
-                  estado: (
-                    <>
-                      <div>
-                        <Chip
-                          className="w-10"
-                          color={`${fila.estado === "pendiente" ? "danger" : "success"
+                    ...fila,
+                    estado: (
+                      <>
+                        <div>
+                          <Chip
+                            className="w-10"
+                            color={`${
+                              fila.estado === "pendiente" ? "danger" : "success"
                             }`}
-                        >
-                          {" "}
-                          {fila.estado}
-                        </Chip>
-                      </div>
-                    </>
-                  ),
+                          >
+                            {" "}
+                            {fila.estado}
+                          </Chip>
+                        </div>
+                      </>
+                    ),
 
-                  acciones: (
-                    <>
-                      <Button
-                        onClick={() =>
-                          handleResolverIncidencias(fila.id, fila)
-                        }
-                      >
-                        resolver
-                      </Button>
-                    </>
-                  ),
-                }))
+                    acciones: (
+                      <>
+                        <Button
+                          onClick={() =>
+                            handleResolverIncidencias(fila.id, fila)
+                          }
+                        >
+                          resolver
+                        </Button>
+                      </>
+                    ),
+                  }))
             }
           />
         </div>
