0f33f7c71603bddd291247b26ed016500f2ec5a5
emersson <emerssonramirez510@gmail.com>
2024-10-26 23:16:49 -0500

    pagina y formulario para reportar novedades

---
+++ frontend/src/pages/ReportarNovedades.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "../components/layouts/Layout";
+import { ReportarNovedadForm } from "../components/formularios/ReportarNovedadForm";
+
+export const ReportarNovedadPage = () => {
+  return (
+    <>
+      <Layout>
+        <ReportarNovedadForm />
+      </Layout>
+    </>
+  );
+};
