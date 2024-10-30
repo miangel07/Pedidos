509c998cf6b79dc25118e35afdc89835ac8dbff2
miangel07 <miguelosoriorojas063@gmail.com>
2024-10-30 01:48:09 -0500

    agregando filtros

--- frontend/src/pages/ListarSolicitudIDPages.jsx
+++ frontend/src/pages/ListarSolicitudIDPages.jsx
@@ -1,13 +1,17 @@
-import React, { useContext, useEffect } from 'react'
-import { Layout } from '../components/layouts/Layout'
-import { useQuerySolicitudesId } from '../hooks/Solicitud'
-import { Table } from '../components/Nextui/Table'
+import React, { useContext, useEffect, useState } from 'react';
+import { Layout } from '../components/layouts/Layout';
+import { useQuerySolicitudesId } from '../hooks/Solicitud';
+import { Table } from '../components/Nextui/Table';
 import { Eye } from 'lucide-react';
 import { Chip } from '@nextui-org/react';
 import { toast } from 'react-toastify';
+
 const ListarSolicitudIDPages = () => {
   const { solicitudUser } = useQuerySolicitudesId();
-console.log(solicitudUser)
+  const [estado, setEstado] = useState("");
+  const [solicitudesFiltradas, setSolicitudesFiltradas] = useState(solicitudUser);
+
+
 
   const colorEstado = {
     pendiente: "warning",
@@ -16,36 +20,65 @@ console.log(solicitudUser)
     completado: "success",
     reprogramado: "warning",
     cancelado: "danger",
+  };
 
-  }
-  const column = ["id", "descripcion del Producto", "direccion de entrega", "direccion de recogida", "domiciliario", "estado", "fecha", "acciones"]
+  const Estados = ["pendiente", "asignado", "en_curso", "completado", "reprogramado", "cancelado"];
+  const column = ["id", "descripcion del Producto", "direccion de entrega", "direccion de recogida", "domiciliario", "estado", "fecha", "acciones"];
+
   const handleVer = (items) => {
-    console.log(items)
-  }
+    console.log(items);
+  };
+
+  useEffect(() => {
+    if (estado) {
+      
+      const solicitudesFiltradas = solicitudUser.filter(item => item.estado === estado);
+      setSolicitudesFiltradas(solicitudesFiltradas);
+    } else {
+     
+      setSolicitudesFiltradas(solicitudUser);
+    }
+  }, [estado, solicitudUser]); 
+
   return (
     <Layout>
-      <Table columns={column} data={solicitudUser?.map((items) => ({
+     
+        <select
+          className="bg-white border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none p-2 mb-5"
+          value={estado}
+          onChange={(e) => setEstado(e.target.value)}
+        >
+          <option value="">Todos</option>
+          {Estados.map(tipo => (
+            <option key={tipo} value={tipo}>
+              {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+            </option>
+          ))}
+        </select>
+     
+      <Table columns={column} data={solicitudesFiltradas?.map((items) => ({
         id: items.id,
         descripcion_Producto: items.descripcion_Producto,
         direccion_entrega: items.direccion_entrega,
         direccion_recogida: items.direccion_recogida,
         domiciliario_nombre: items.domiciliario_nombre,
-        estado: (<Chip
-          className="w-10"
-          color={colorEstado[items.estado]}
-        >
-          {items.estado}
-        </Chip>),
+        estado: (
+          <Chip
+            className="w-10"
+            color={colorEstado[items.estado]}
+          >
+            {items.estado}
+          </Chip>
+        ),
         fecha: items.fecha,
         acciones: (
           <>
             <Eye size={35} className='cursor-pointer' onClick={() => handleVer(items)} />
           </>
         ),
-      })
-      )} itemsPerPage={5} />
+      }))} itemsPerPage={5} />
     </Layout>
-  )
-}
+  );
+};
 
-export default ListarSolicitudIDPages
+export default ListarSolicitudIDPages;
--- frontend/src/pages/UsuarioPages.jsx
+++ frontend/src/pages/UsuarioPages.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Layout } from '../components/layouts/Layout';
 import { useUserMutation } from '../hooks/Usuario';
 import { Table } from "../components/Nextui/Table";
@@ -8,13 +8,14 @@ import UsuarioFormulario from '../components/formularios/UsuarioFormulario';
 import ButtonNext from '../components/Nextui/ButtonNext';
 import { toast } from 'react-toastify';
 import { axiosCliente } from '../service/axios';
-import { AuthContext } from "../context/AuthContext";
+
+
 const UsuarioPages = () => {
     const { usuario, refress } = useUserMutation();
     const [isOpen, setIsOpen] = useState(false);
     const [dataUsuario, setDataUsuario] = useState(null);
-    const { authData } = useContext(AuthContext);
-  console.log(authData)
+    const [tipoUsuarioSeleccionado, setTipoUsuarioSeleccionado] = useState(""); 
+    const [usuariosFiltrados, setUsuariosFiltrados] = useState([]); 
 
     const columnas = [
         "id",
@@ -26,52 +27,73 @@ const UsuarioPages = () => {
         "acciones"
     ];
 
-    const handleEdit = async (data) => {
+    const tiposDeUsuario = ["administrador", "negocio", "particular", "domiciliario"]; 
 
+    const handleEdit = async (data) => {
         setIsOpen(true);
         setDataUsuario(data);
-
     };
+
     const handleEstado = async (data) => {
         try {
             const estadoUser = data.estado === "activo" ? "inactivo" : "activo";
             const response = await axiosCliente.put(`usuarioEstado/${data.id}`, { estado: estadoUser });
             if (response.status === 200) {
-                await refress()
-                toast.success(`${response.data.mensaje}`)
+                await refress();
+                toast.success(`${response.data.mensaje}`);
             }
         } catch (error) {
-            console.error(error.message)
+            console.error(error.message);
         }
+    };
 
-    }
     const closeModal = () => {
         setDataUsuario(null);
         setIsOpen(false);
     };
 
+
+    useEffect(() => {
+        if (tipoUsuarioSeleccionado) {
+            setUsuariosFiltrados(usuario.filter(fila => fila.TipoUsuario === tipoUsuarioSeleccionado));
+        } else {
+            setUsuariosFiltrados(usuario);
+        }
+    }, [tipoUsuarioSeleccionado, usuario]);
+
     return (
         <Layout>
-            {
-                isOpen && (
-                    <Modals visible={isOpen} title={dataUsuario ? "Editar Usuario" : "Registrar Usuario"} closeModal={closeModal} >
-                        <UsuarioFormulario
-                            data={dataUsuario}
-                            closeModal={setIsOpen}
-                        />
-                    </Modals>
-                )
-            }
-            <div className='mb-5'>
+            {isOpen && (
+                <Modals visible={isOpen} title={dataUsuario ? "Editar Usuario" : "Registrar Usuario"} closeModal={closeModal}>
+                    <UsuarioFormulario
+                        data={dataUsuario}
+                        closeModal={setIsOpen}
+                    />
+                </Modals>
+            )}
+            <div className='mb-5 w-full justify-around flex'>
                 <ButtonNext className="mb-5" onClick={() => setIsOpen(true)}>
                     Agregar Usuario
                 </ButtonNext>
+                <select
+                    className="bg-white border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none p-2 mb-5"
+                    value={tipoUsuarioSeleccionado}
+                    onChange={(e) => setTipoUsuarioSeleccionado(e.target.value)}
+                >
+                    <option value="">Todos</option>
+                    {tiposDeUsuario.map(tipo => (
+                        <option key={tipo} value={tipo}>
+                            {tipo.charAt(0).toUpperCase() + tipo.slice(1)}
+                        </option>
+                    ))}
+                </select>
             </div>
+
 
             <Table
                 columns={columnas}
                 data={
-                    usuario?.map((filas) => ({
+                    usuariosFiltrados?.map((filas) => ({
                         id: filas.id,
                         nombre: filas.nombre,
                         TipoUsuario: filas.TipoUsuario,
@@ -101,7 +123,7 @@ const UsuarioPages = () => {
                                     className={`${filas.estado === "inactivo" ? "bg-danger" : "bg-warning"} text-white cursor-pointer`}
                                     onClick={() => handleEstado(filas)}
                                 >
-                                    {filas == "activo" ? "Desactivar" : "Activar"}
+                                    {filas.estado === "activo" ? "Desactivar" : "Activar"}
                                 </Chip>
                             </div>
                         ),
